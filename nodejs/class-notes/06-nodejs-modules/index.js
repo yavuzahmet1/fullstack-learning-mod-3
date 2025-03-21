6bdf756a7ef16dd7b52a60616c605446bbf2ad17
1yavuz <1yavuzahmet@gmail.com>
2025-03-21 22:11:50 +0300

    lesson section 1

---
+++ nodejs/class-notes/06-nodejs-modules/index.js
@@ -0,0 +1,26 @@
+"use strict"
+
+
+
+console.log("hello fs18")
+
+require("./module/module") //! require metodu başka bir js dosyasını içeri aktarmak için kullanılır
+
+// const testFunc= require("./module/module")
+
+// testFunc() //*tek fonksiyon import etme
+
+
+/* -------------------------------------------------------------------------- */
+
+// const {imrenBurc:dogumGünü,defneBurc,imren}= require("./module/module")
+// console.log(imren)
+// // imrenBurc()
+// // defneBurc()
+// dogumGünü()
+
+
+/* -------------------------------------------------------------------------- */
+// const [tekRakam,ciftRakam]=require("./module/module")
+// tekRakam()
+// ciftRakam()
---
+++ nodejs/class-notes/06-nodejs-modules/module/module.js
@@ -0,0 +1,40 @@
+"use strict"
+
+console.log("module çalışıyor")
+
+/* ------------------------ tek fonksiyon export etme ----------------------- */
+
+// const testFunc = function(){
+//     console.log("this is a function")
+// }
+// module.exports=testFunc
+
+
+/* --------------- Birden fazla değer veya fonskiyon gönderme --------------- */
+
+// const imren=14.01
+
+// const dogumGünü=function(){
+//     console.log("imren oğlak burcudur")
+// }
+// const dogumGünü2=function(){
+//     console.log("defne koç burcudur")
+// }
+// // module.exports={dogumGünü,dogumGünü2}
+// module.exports={
+//     imrenBurc:dogumGünü,
+//     defneBurc:dogumGünü2,
+//     imren
+// }
+
+/* -------------------------- Array olarak gönderme ------------------------- */
+
+// const tekRakam = function(){
+//     console.log("1,3,5,7,9")
+// }
+// const ciftRakam= function(){
+//     console.log("0,2,4,6,8")
+// }
+// module.exports=[tekRakam,ciftRakam]
+
+

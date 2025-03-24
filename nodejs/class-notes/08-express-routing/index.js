8ed80c739c355789d961e7640c70053e1a06b93f
1yavuz <1yavuzahmet@gmail.com>
2025-03-24 22:19:10 +0300

    express js setup

---
+++ nodejs/class-notes/08-express-routing/index.js
@@ -0,0 +1,12 @@
+"use strict"
+//*EXPRESS & ROUTING
+
+//initial commands
+//* npm init -y
+//* npm i -D nodemon
+//* pnpm add express
+//* echo PORT=8000>.env
+
+//express start
+const express = require("express");
+const app = express();

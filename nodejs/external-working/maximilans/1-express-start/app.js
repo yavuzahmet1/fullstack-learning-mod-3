4cb3701a0d3b2b91a51b016e6a41eeca1a161762
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-03-26 19:01:35 +0300

    example middleware added

--- nodejs/external-working/maximilans/1-express-start/app.js
+++ nodejs/external-working/maximilans/1-express-start/app.js
@@ -4,6 +4,15 @@ const express=require("express")
 
 const app=express()
 
-const server=http.createServer();// Sunucu örneği oluşturur
+app.use((req,res,next)=>{
+    console.log("In the middleware!");
+    next()// çağırarak bir sonraki middleware'e geçmesi ya da bir yanıt göndermesi gerekir.
+})
+
+app.use((req,res,next)=>{
+    console.log("another  middleware!");
+})
+
+const server=http.createServer(app);// Sunucu örneği oluşturur
 
 server.listen(3000);// 3000 portunda dinlemeye başlar ve sunucuyu 3000 portunda başlatır.

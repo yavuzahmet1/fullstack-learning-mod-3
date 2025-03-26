c1c8752f5a3dfb26451ce7acec503163007c85ad
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-03-26 21:35:59 +0300

    route created

--- nodejs/class-notes/9-routes-middlewares/router.js
+++ nodejs/class-notes/9-routes-middlewares/router.js
@@ -6,4 +6,17 @@ const app=express()
 require("dotenv").config();
 const PORT=process.env.PORT??8000;
 
+//*Router is special app for URL control is Express.js
+
+const router=express.Router();
+
+// router.get("/", (req,res)=>res.send({method:"GET"}))
+
+router.route("/")
+.get((req,res)=>res.send({method:"GET"}))
+.post((req,res)=>res.send({method:"POST"}))
+.put((req,res)=>res.send({method:"PUT"}))
+
+app.use(router);
+
 app.listen(PORT,()=>console.log("running at:http//127.0.0.1:"+PORT))

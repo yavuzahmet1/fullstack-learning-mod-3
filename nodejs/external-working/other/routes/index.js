5ee2a074dd4f44b1d2ab24246fabd74285998892
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-03-27 22:41:06 +0300

    async noted for node js

--- nodejs/external-working/other/routes/index.js
+++ nodejs/external-working/other/routes/index.js
@@ -2,3 +2,15 @@ const express=require("express")
 const app=express();
 
 app.listen(3000) //3.20 de kaldım https://www.youtube.com/watch?v=SccSCuHhOw0
+
+// Senkron Programlama (Synchronous Programming):
+
+
+// Senkron programlama en basit tanımıyla işlemlerin sırasıyla yapılmasıdır. Önce bir dosya okuyalım, sonra bir veritabanı işlemi yapalım ve sonrasında da bir http isteği gönderelim şeklinde basit bir şekilde çalışır. Beklemekten sıkılmaz, başka işlerin aciliyetiyle ilgilenmez. Bir işlem yapılırken diğer işlemler bloklanır.
+
+// Asenkron Programlama (Asynchronous Programming):
+
+
+// Asenkron programlama ise uzun süren bir işlemi beklemeden diğer işlemlere devam edebilmektir, işlemlerin sırasıyla devam etme zorunluluğu yoktur. Önce bir dosya okuyalım, aaa ama bu dosya okumak uzun sürüyor, eee ne yapalım o zaman? Bu işlemin bitmesini beklemeden diğer işleme geçebiliriz. Bir işlem yapılırken diğer işlemler bloklanmaz.
+
+// avascript single thread, yani aynı anda sadece ve sadece tek bir işlem yapabilir. 

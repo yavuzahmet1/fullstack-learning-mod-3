b4cd5ff383b21ec22c86e4f87f3e2a9e47c42308
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-03-27 22:41:30 +0300

    async note added

--- nodejs/external-working/patika/1-async-nodejs/index.js
+++ nodejs/external-working/patika/1-async-nodejs/index.js
@@ -12,3 +12,17 @@ const server = createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+// Senkron Programlama (Synchronous Programming):
+
+
+// Senkron programlama en basit tanımıyla işlemlerin sırasıyla yapılmasıdır. Önce bir dosya okuyalım, sonra bir veritabanı işlemi yapalım ve sonrasında da bir http isteği gönderelim şeklinde basit bir şekilde çalışır. Beklemekten sıkılmaz, başka işlerin aciliyetiyle ilgilenmez. Bir işlem yapılırken diğer işlemler bloklanır.
+
+// Asenkron Programlama (Asynchronous Programming):
+
+
+// Asenkron programlama ise uzun süren bir işlemi beklemeden diğer işlemlere devam edebilmektir, işlemlerin sırasıyla devam etme zorunluluğu yoktur. Önce bir dosya okuyalım, aaa ama bu dosya okumak uzun sürüyor, eee ne yapalım o zaman? Bu işlemin bitmesini beklemeden diğer işleme geçebiliriz. Bir işlem yapılırken diğer işlemler bloklanmaz.
+
+// avascript single thread, yani aynı anda sadece ve sadece tek bir işlem yapabilir. 
+
+// peki bu işlemler bittiğinde bunun sıralaması nasıl yapılıyor? İşte burada devreye event-loop girer

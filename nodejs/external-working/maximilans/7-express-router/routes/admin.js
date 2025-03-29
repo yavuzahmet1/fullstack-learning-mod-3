92961d2bf4eb7e0fbda00418c4b0fddb87aca339
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-03-29 23:48:58 +0300

    Product and add-product methods added and with routes exported

---
+++ nodejs/external-working/maximilans/7-express-router/routes/admin.js
@@ -0,0 +1,47 @@
+const express=require("express");
+const router=express.Router();
+
+router.get("/add-product",(req,res,next)=>{
+    res.send("<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button>")
+    });
+
+
+router.post("/product",(req,res,next)=>{
+    console.log(req.body);
+    res.redirect("/");
+    });
+
+module.exports=router;
+
+//? 1. router.get() Kullanımı
+
+// router.get("/add-product", (req, res, next) => {
+//   res.send("<form>...</form>");
+// });
+// Sadece GET isteklerine yanıt verir
+// Belirli bir route için kullanılır
+// HTTP metoduna özel işlemlerde tercih edilir
+
+//? 2. router.use() Kullanımı
+// router.use("/add-product", (req, res, next) => {
+//   res.send("<form>...</form>");
+// });
+// Tüm HTTP metodlarına (GET, POST, PUT, DELETE vb.) yanıt verir
+// Middleware olarak kullanılabilir
+// Daha genel amaçlı işlemler için uygundur
+
+// HTTP de Method özgürlüğünde router.get() önde,
+// Middleware desteğinde router.use() tam destekli,
+// performansda router.get() daha hızlı.
+
+// router.use() =>Logging, authentication gibi genel işlemlerde kullanabiliriz.
+
+//?Performans ve Best Practice
+// Metod özgüllüğü her zaman daha iyidir - get(), post() gibi metodları kullanın
+
+//* use() daha çok:
+// Middleware'ler için
+// Route gruplama için
+// Tüm metodlara aynı yanıtı vermeniz gereken durumlar için
+
+// Sektör standardı: RESTful API'lerde metod-spesifik route'lar (get, post, put, delete) kullanmak daha yaygın ve tercih edilen yöntemdir.

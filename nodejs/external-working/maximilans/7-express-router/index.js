a70506a4a9f0392d7779e0796d4592091cf7ad90
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-03-30 01:05:27 +0300

    path import and used not foun  sendFile

--- nodejs/external-working/maximilans/7-express-router/index.js
+++ nodejs/external-working/maximilans/7-express-router/index.js
@@ -1,3 +1,4 @@
+const path =require("path")
 const express=require("express");
 const app=express();
 const PORT=3000;
@@ -11,7 +12,7 @@ app.use('/admin',adminRoutes)
 app.use(shopRoutes)
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page Not found</h1>')
+    res.status(404).sendFile(path.join(__dirname,"views","404.html"))
 })
 
 app.listen(PORT,()=>{

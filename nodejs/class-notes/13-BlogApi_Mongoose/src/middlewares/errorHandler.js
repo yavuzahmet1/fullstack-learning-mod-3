5f0c4c53b8027f79c23e4329dc6d0a8bc5f14616
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-04-07 21:24:05 +0300

    errror hanler added

---
+++ nodejs/class-notes/13-BlogApi_Mongoose/src/middlewares/errorHandler.js
@@ -0,0 +1,12 @@
+"use strict"
+
+//?ERROR HANDLER
+
+module.exports = (err, req, res, next) => {
+    console.log("Errorhandler Worked");
+    const customErrorCode = res?.customErrorCode || 500;
+    res.status(customErrorCode).send({
+        error: true,
+        message: err.message,
+    });
+};

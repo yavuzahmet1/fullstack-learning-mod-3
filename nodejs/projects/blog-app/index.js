ddebe8d5f37f1f142a0d8432756acfd81bb11e3d
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-04-12 23:12:24 +0300

    feat: set up core server infrastructure
    - Added Express.js initialization with express.json()
    - Implemented database connection in index.js
    - Configured port listening with environment variables
    - Added basic server health check route

---
+++ nodejs/projects/blog-app/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+//*Express.js uygulamalarında gelen HTTP request'lerin body kısmındaki JSON verilerini** otomatik olarak parse eden built-in bir middleware'dir.
+app.use(express.json())
+
+//*Amacı: Express'te async/await kullanılan route'larda oluşan hataları otomatik yakalar ve hata middleware'ine iletir.
+require("express-async-errors")
+
+// const dbConnection = require("./src/dbConnection")
+// dbConnection()
+require("./src/dbConnection")();
+
+app.all("/", (req, res) => res.send("Welcome To Blog App"))
+
+
+app.use(require('./src/middlewares/errorHanler'))
+
+app.listen(PORT, () => console.log(`Running at at http://127.0.0.1:${PORT}`))

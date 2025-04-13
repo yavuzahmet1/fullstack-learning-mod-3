86e0d8429dd7bc5eb9f9d6e08b4016d929d3d9e9
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-04-13 21:41:57 +0300

    refactor(routes): improve endpoint naming consistency
    - Renamed category routes:
      * /blogs → /categories
      * /blogs/:id → /categories/:id
    - Renamed post routes:
      * /blogs → /posts
      * /blogs/:id → /posts/:id
    - Updated all related controller references

--- nodejs/projects/blog-app/src/routes/blog.router.js
+++ nodejs/projects/blog-app/src/routes/blog.router.js
@@ -1,21 +1,21 @@
 const router = require("express").Router();
 const { blogCategory, blogPost } = require("../controllers/blog.controller");
 
-router.route("/blogs")
+router.route("/blogs/category")
     .get(blogCategory.list)
     .post(blogCategory.create);
 
-router.route("/blogs/:id")
+router.route("/blogs/category/:id")
     .get(blogCategory.read)
     .put(blogCategory.update)
     .patch(blogCategory.update)
     .delete(blogCategory.delete)
 
-router.route("/blogs")
+router.route("/blogs/post")
     .get(blogPost.list)
     .post(blogPost.create);
 
-router.route("/blogs/:id")
+router.route("/blogs/post/:id")
     .get(blogPost.read)
     .put(blogPost.update)
     .patch(blogPost.update)

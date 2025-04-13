f2f3e045a37df915149c1bc12db1fa3636fd14e5
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-04-13 21:37:31 +0300

    feat(blog.controller): implement full CRUD for BlogPost
    - Added all CRUD operations:
      * list: GET /blog-posts
      * create: POST /blog-posts
      * read: GET /blog-posts/:id
      * update: PUT/PATCH /blog-posts/:id
      * delete: DELETE /blog-posts/:id
    - Uses Mongoose methods:
      * find(), create(), findById()
      * findOneAndUpdate({ new: true })
      * deleteOne() with 204/404 handling
    - Consistent response structure

--- nodejs/projects/blog-app/src/controllers/blog.controller.js
+++ nodejs/projects/blog-app/src/controllers/blog.controller.js
@@ -46,3 +46,51 @@ module.exports.blogCategory = {
     },
 
 }
+
+module.exports.blogPost = {
+    list: async (req, res) => {
+        const result = await BlogPost.find();
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+    create: async (req, res) => {
+        const result = await BlogPost.create(req.body);
+        res.status(201).send({
+            error: false,
+            result
+        })
+    },
+    read: async (req, res) => {
+        // const result = await BlogCategory.findOne({ _id: req.params.id });
+        const result = await BlogPost.findById(req.params.id);
+        res.status(200).send({
+            error: false,
+            result
+        })
+    },
+    update: async (req, res) => {
+        // const result = await BlogCategory.updateOne({...filter},{...data},{...option});
+        // const result = await BlogCategory.updateOne({ _id: req.params.id }, req.body);
+        const result = await BlogPost.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true });
+        res.status(200).send({
+            error: false,
+            result,
+            // new: await BlogCategory.findById(req.params.id) findOneAndUpdate kullanılırsa gerek yok
+        })
+    },
+    delete: async (req, res) => {
+
+        const result = await BlogPost.deleteOne({ _id: req.params.id });
+
+        if (result?.deletedCount) {
+            res.sendStatus(204);
+        } else {
+            res.errorStatus = 404;
+            throw new Error("Data is not found or already deleted")
+        }
+    },
+
+}
+

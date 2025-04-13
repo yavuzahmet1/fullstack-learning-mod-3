ed25cb88e0dce4d1f886cf5aaa699af4b8f64b39
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-04-13 16:45:46 +0300

    feat(routes): add POST endpoint for blog categories
    - Register create controller in blog router
    - Enable POST /blogs route for category creation
    - Maintains RESTful routing structure

--- nodejs/projects/blog-app/src/routes/blog.router.js
+++ nodejs/projects/blog-app/src/routes/blog.router.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const blogCategory = require("../controllers/blog.controller");
 
-router.route("/blogs").get(blogCategory.list);
+router.route("/blogs")
+    .get(blogCategory.list)
+    .post(blogCategory.create);
 
 module.exports = router;

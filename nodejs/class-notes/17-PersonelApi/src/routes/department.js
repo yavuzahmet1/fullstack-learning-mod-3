495e8164fa24c40022c398913f1c89b8546e26be
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-04-16 21:50:39 +0300

    feat(routes): configure department CRUD routes
    - GET    /           → department.list()
    - POST   /           → department.create()
    - GET    /:id        → department.read()
    - PUT    /:id        → department.update()
    - PATCH  /:id        → department.update()
    - DELETE /:id        → department.delete()

--- nodejs/class-notes/17-PersonelApi/src/routes/department.js
+++ nodejs/class-notes/17-PersonelApi/src/routes/department.js
@@ -2,8 +2,18 @@
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-const router = require('express').Router()
-/* ------------------------------------------------------- */
+const router = require('express').Router();
+const department = require("../controllers/department");
+
+router.route("/")
+    .get(department.list)
+    .post(department.create)
+
+router.route("/:id")
+    .get(department.read)
+    .put(department.update)
+    .patch(department.update)
+    .delete(department.delete)
 
 
 

62894d3510413882922dd0b863ff1bfe566deeeb
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-04-16 21:16:26 +0300

    feat: add personnel create controller

--- nodejs/class-notes/17-PersonelApi/src/controllers/personnel.js
+++ nodejs/class-notes/17-PersonelApi/src/controllers/personnel.js
@@ -2,3 +2,30 @@
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
+const { getModelList, getModelListDetails } = require("../middlewares/queryHandler")
+const Department = require("../configs/dbConnection");
+
+module.exports = {
+    list: async (req, res) => {
+        // const result = await Department.find()
+        const result = await getModelList(Department)
+        res.status(200).send({
+            error: false,
+            details: await getModelListDetails(Department),
+            result,
+        })
+    },
+    create: async (req, res) => {
+
+    },
+    read: async (req, res) => {
+
+    },
+    update: async (req, res) => {
+
+    },
+    delete: async (req, res) => {
+
+    }
+
+}

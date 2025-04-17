194b27116852feaacab963cca139ea7373be861f
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-04-18 00:27:04 +0300

    feat: 🍖 add cookie-session middleware
    - Secure session storage via encrypted cookies 🛡️
    - 3-day expiry (maxAge: 1000 * 60 * 60 * 24 * 3)
    - Secret key from `PASS_SALT` env 🔑

--- nodejs/projects/blog-app/index.js
+++ nodejs/projects/blog-app/index.js
@@ -7,28 +7,38 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
 //*Express.js uygulamalarında gelen HTTP request'lerin body kısmındaki JSON verilerini** otomatik olarak parse eden built-in bir middleware'dir.
-app.use(express.json())
+app.use(express.json());
 
 //*Amacı: Express'te async/await kullanılan route'larda oluşan hataları otomatik yakalar ve hata middleware'ine iletir.
-require("express-async-errors")
+require("express-async-errors");
 
 // const dbConnection = require("./src/dbConnection")
 // dbConnection()
 require("./src/dbConnection")();
 
+//session-cookies
+const session = require("cookie-session");
+app.use(
+  session({
+    secret: process.env.PASS_SALT,
+    maxAge: 1000 * 60 * 60 * 24 * 3, //sessionun süresini belirlediğimiz alan,sessionlara süre verdiğimizde cookie oluyor
+  })
+);
+
 //main-route
 app.all("/", (req, res) => {
-    console.log(req.session)
-    res.send("Welcome To Blog App")
+  res.send({
+    message: "Welcome To Blog App",
+    session: req.session,
+  });
 });
 
 //blog-routes
-app.use("/blogs", require("./src/routes/blog.router"))
+app.use("/blogs", require("./src/routes/blog.router"));
 
-app.use("/users", require("./src/routes/user.router"))
-
+app.use("/users", require("./src/routes/user.router"));
 
 //error handler
-app.use(require('./src/middlewares/errorHanler'))
+app.use(require("./src/middlewares/errorHanler"));
 
-app.listen(PORT, () => console.log(`Running at at http://127.0.0.1:${PORT}`))
+app.listen(PORT, () => console.log(`Running at at http://127.0.0.1:${PORT}`));

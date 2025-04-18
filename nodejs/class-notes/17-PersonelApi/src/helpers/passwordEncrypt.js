382fdc771001208031e49148b005e167b4df4c57
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-04-18 19:29:48 +0300

    🎨 style(code): Improve code formatting
    - Apply Prettier/ESLint rules for consistency
    - Adjust indentation, line breaks, and punctuation
    - Add whitespace for better readability in complex functions

--- nodejs/class-notes/17-PersonelApi/src/helpers/passwordEncrypt.js
+++ nodejs/class-notes/17-PersonelApi/src/helpers/passwordEncrypt.js
@@ -1,26 +1,23 @@
-"use strict"
-/* -------------------------------------------------------
-    EXPRESS - Personnel API
-------------------------------------------------------- */
+"use strict";
+
 // passwordEncrypt():
 
-const { 
-    crypto, 
-    SECRET_KEY: keyCode, 
-    LOOP_COUNT: loopCount, 
-    CHAR_COUNT: charCount, 
-    ENC_TYPE: encType 
+const {
+  crypto,
+  SECRET_KEY: keyCode,
+  LOOP_COUNT: loopCount,
+  CHAR_COUNT: charCount,
+  ENC_TYPE: encType,
 } = {
-    crypto: require('node:crypto'),
-    ...process.env
+  crypto: require("node:crypto"),
+  ...process.env,
 };
 
 const loopCountNum = Number(loopCount);
 const charCountNum = Number(charCount);
 
-
-
 module.exports = (password) => {
-
-    return crypto.pbkdf2Sync(password, keyCode, loopCountNum, charCountNum, encType).toString('hex')
-}
+  return crypto
+    .pbkdf2Sync(password, keyCode, loopCountNum, charCountNum, encType)
+    .toString("hex");
+};

e18cb2787820df6f5bc78d9e3139e4df74849011
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-04-19 22:46:19 +0300

    ✨ feat(department): Secure CRUD with isAdmin/isAdminOrLead

--- nodejs/class-notes/17-PersonelApi/src/routes/department.js
+++ nodejs/class-notes/17-PersonelApi/src/routes/department.js
@@ -2,14 +2,19 @@
 
 const router = require("express").Router();
 const department = require("../controllers/department");
+const {
+  isLogin,
+  isAdmin,
+  isAdminOrLead,
+} = require("../middlewares/permission");
 
 router.route("/").get(department.list).post(department.create);
 
 router
   .route("/:id")
-  .get(department.read)
-  .put(department.update)
-  .patch(department.update)
-  .delete(department.delete);
+  .get(isLogin, department.read)
+  .put(isAdminOrLead, department.update)
+  .patch(isAdminOrLead, department.update)
+  .delete(isAdmin, department.delete);
 
 module.exports = router;

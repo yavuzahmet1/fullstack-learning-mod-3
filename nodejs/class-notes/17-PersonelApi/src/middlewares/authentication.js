0cef271cef2e30851ee01084fb1d28dca4f4042c
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-04-19 22:41:30 +0300

    feat(auth): Add token validation & user population

--- nodejs/class-notes/17-PersonelApi/src/middlewares/authentication.js
+++ nodejs/class-notes/17-PersonelApi/src/middlewares/authentication.js
@@ -5,5 +5,12 @@ module.exports = async (req, res, next) => {
   // Get token from header
   const auth = req.headers?.authorization || null;
   const tokenKey = auth ? auth.split("")[1] : null;
+
+  if (tokenKey && tokenKey[0] == "Token") {
+    const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
+      "userId"
+    );
+    if (tokenData) req.user = tokenData.userId;
+  }
   next();
 };

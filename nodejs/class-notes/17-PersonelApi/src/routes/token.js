e9414c298f5b92e0d33e3d8b52b78a1dfe5e9fc4
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-04-19 22:48:35 +0300

    feat(tokens): Add admin protection & delete endpoint

--- nodejs/class-notes/17-PersonelApi/src/routes/token.js
+++ nodejs/class-notes/17-PersonelApi/src/routes/token.js
@@ -2,9 +2,13 @@
 
 const router = require("express").Router();
 const { list, create, read, deletee } = require("../controllers/token");
+const { isAdmin } = require("../middlewares/permission");
 
 router.route("/").get(list).post(create);
 
-router.route("/:id").get(read).delete(deletee);
+router
+  .route("/:id")
+  .get(read)
+  .delete((req, res, next) => {}, deletee);
 
 module.exports = router;
